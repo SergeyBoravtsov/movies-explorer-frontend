1aa4cf47c42fe30627f28de28c59f00b19ba1676
Sergey Boravtsov <sergeyboravtsov@yandex.ru>
2022-06-29 00:39:55 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies/';
-export const MAIN_API_URL = 'https://api.sergbor.movies.nomoredomains.rocks';
+export const MAIN_API_URL = 'https://movies-explorer-api-sergeyboravtsov.vercel.app/';
+// export const MAIN_API_URL = 'https://api.sergbor.movies.nomoredomains.rocks';
 // export const MAIN_API_URL = 'http://localhost:3001';
 
 export const ERROR_GENERAL_TEXT = 'Произошла ошибка';
